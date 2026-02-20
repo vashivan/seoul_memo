a892fc97bcba7099e711d2c9b1898927661c89a2
Ivan Vashchuk <vaniavaschuk@gmail.com>
2026-02-21 01:34:10 +0900

    prisma, db, neon, main, order

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaClient } from "@/app/generated/prisma";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
 const connectionString = process.env.DATABASE_URL;
